e8d1c5915ff066639e692722a1914d7f0c26ff20
nishang.jx <nishang.jx@autonavi.com>
2020-03-29 15:00:07 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared/env.dart';
+import 'demo.dart';
+import 'MyHomePage.dart';
+import 'Perspective.dart';
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+
+  static String _pkg = "parallax_travel_cards_list";
+  static String get pkg => Env.getPackage(_pkg);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+    return MaterialApp(
+      home: MyPerspectivePage(),
+    );
+  }
+}
